73f2011dada8c397615e8f4c8f2f5726b5fc3aa9
Denis Kipyegon <57701113+kipyek@users.noreply.github.com>
2025-08-14 08:32:44 +0300

    initial

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useRef, useState } from 'react'
+
+function App() {
+  const [time, setTime] = useState(0)
+  const [isRunning, setIsRunning] = useState(false)
+  const timeRef = useRef(null)
+
+  const timerController = () => {
+    if (isRunning) {
+      clearInterval(timeRef.current)
+      timeRef.current = null
+    } else {
+      timeRef.current = setInterval(() => {
+        setTime((prev) => prev + 1)
+      }, 1000)
+    }
+
+    setIsRunning(!isRunning)
+
+  }
+
+  return (
+    <div className='max-w-xl mx-auto bg-white p-4 rounded-lg shadow-lg items-center justify-center'>
+      <h1 className="text-2xl font-bold text-center">Timer : <strong>{time}</strong></h1>
+      <button onClick={timerController} className='bg-green-500 px-4 py-2 rounded'>{isRunning ? "Pause" : "Start"}</button>
+    </div>
+  )
+}
+
+export default App
