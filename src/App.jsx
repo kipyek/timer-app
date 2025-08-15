2d83428eee1dbde3942367c7a196e10e595ef43f
Denis Kipyegon <57701113+kipyek@users.noreply.github.com>
2025-08-15 22:27:42 +0300

    added reset and title

--- src/App.jsx
+++ src/App.jsx
@@ -19,10 +19,21 @@ function App() {
 
   }
 
+  const reset = () => {
+    setTime(0)
+    clearInterval(timeRef.current)
+    timeRef.current = 0;
+    setIsRunning(false)
+  }
+
   return (
     <div className='max-w-xl mx-auto bg-white p-4 rounded-lg shadow-lg items-center justify-center'>
       <h1 className="text-2xl font-bold text-center">Timer : <strong>{time}</strong></h1>
-      <button onClick={timerController} className='bg-green-500 px-4 py-2 rounded'>{isRunning ? "Pause" : "Start"}</button>
+     
+      <div className="display-flex mx-auto">
+         <button onClick={timerController} className='bg-green-500 px-4 py-2 rounded'>{isRunning ? "Pause" : "Start"}</button>
+         <button onClick={reset} className='bg-red-500 px-4 py-2 rounded ml-4'>Reset</button>
+      </div>
     </div>
   )
 }
